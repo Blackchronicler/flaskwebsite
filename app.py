182c7edf6717e25514afea71691daf7346ca631c
Blackchronicler <blckchronicler@gmail.com>
2023-09-14 20:29:48 +0200

    Add templates, css and unittest

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, World!"  # return a string
+    return "Welcome to Happy Wheels Network!!"  # return a string
 
 @app.route('/welcome')
 def welcome():
